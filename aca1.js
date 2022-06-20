dc4a6e9c5e422b5f6793c2bb6f0d4dd800c5683a
Haxverdyan <107857369+Haxverdyan@users.noreply.github.com>
2022-06-20 15:58:02 +0400

    Add files via upload

---
+++ aca1.js
@@ -0,0 +1,8 @@
+let elem=document.getElementById('elem')
+	setInterval(()=>elem.style.color='red',2000)
+	setInterval(()=>elem.style.color='',3000)
+	setInterval(()=>elem.innerHTML='BAREV DAVIT JAN',2000)
+	setInterval(()=>elem.innerHTML='Կենսագրություն',3000)
+	setInterval(()=>elem.style.background='blue',2000)
+	setInterval(()=>elem.style.background='',3000)
+
